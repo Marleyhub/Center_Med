0573a10389b7e5a410a8923a1ff2a3f5131bd396
cardoso <gabrielmtg2@gmail.com>
2024-07-04 12:16:55 -0300

    fixing syntax / importing user model to index

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-
-app.listen(3000, ()=> {
-    console.log('server linstening to port :3000')
-})
-
-app.get('/', (req,res) => {
-    res.send('Server Alive')
-})
+    const express = require('express')
+    const mongoose = require('mongoose')
+    const app = express()
+    const User = require('./model-user')
 
 
+    app.use(express.json())
+
+    app.listen(3000, ()=> {
+        console.log('server listening to port :3000')
+    })
+
+    app.get('/', (req,res) => {
+        console.log("foi")
+        console.log(req.body)
+        res.send('Server Alive')
+    })
+
+    app.post('/api', (req,res)=>{
+        console.log(req.body)
+        res.send(req.body)
+    })
 app.post('/api/user', async (req,res)=>{
         try{
-            const user = await User.create(req,body)
+            const user = await User.create(req.body)
             res.status(200).json(user)
+            res.send(req.body)
         } catch (error) {
             res.status(500).json({meessage: error.message})
         }
