e04b58ca3999f08fa7a02e1b9707a8764b5b3de9
cardoso <gabrielmtg2@gmail.com>
2024-07-16 16:11:23 -0300

    Rota getUser adicionada

--- routes/users-route.js
+++ routes/users-route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const User = require('../models/user');
-const {getUsers, createUser, updateUser, deleteUser} = require('../controlers/users-controllers.js');
+const {getUsers, getUser, createUser, updateUser, deleteUser} = require('../controlers/users-controllers.js');
 
 router.get ('/', getUsers);
+router.get('/:id', getUser);
 
 router.post('/create', createUser);
 
