2bc8c8b75c718c8aed31516037193635d50ebff8
cardoso <gabrielmtg2@gmail.com>
2025-01-20 14:42:46 -0300

    adding dependencies

--- routes/appointment-route.js
+++ routes/appointment-route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const {booking, getAppointments} = require('../controllers/appointment-controllers');
+const {booking, getAppointments, printExam} = require('../controllers/appointment-controllers');
 
-router.get('/getAppointments', getAppointments)
+
+router.get('/getAppointments', getAppointments);
 
 router.post('/booking', booking);
+
+router.get('/print', printExam);
 
 module.exports = router;
