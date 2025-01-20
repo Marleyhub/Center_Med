ec0fd3b764b721f0e9a4300dc7be4077a138796c
cardoso <gabrielmtg2@gmail.com>
2025-01-20 14:40:54 -0300

    Adding functions execute loop for the pdf creation

--- controllers/appointment-controllers.js
+++ controllers/appointment-controllers.js
@@ -44,9 +44,59 @@ const path = require('path')
             res.status(500).json({message: err.message})
         }
     }
+
+// printing exam to PDF
+    const printExam = async (req, res) => {
+        try {
+            const token = req.cookies['accessToken'];
+            const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            const userName = payload.name
+
+            const userAppointment = await Appointment.find({user: userName});
+
+            createPdf(userAppointment);
+            res.status(200).json({message: 'PDF Printed'})
+        } catch (err) {
+            res.status(500).json({message: err.message})
+        }
+
+    }
+
+// creating pdf
+    const createPdf = (userAppointment) => {
+        const doc = new PDFDocument();
+
+        const filePath = path.join(__dirname,`appointment_${userAppointment._id}.pdf`)
+
+        doc.pipe(fs.createWriteStream(filePath));
+
+        doc.fontSize(20).text('Appointment Details', { align: 'center' });
+        doc.moveDown();
     
+
+        for (let i = 0; i < userAppointment.length; i ++) {
+
+            // Content
+            doc.fontSize(12);
+            doc.text(`Appointment ID: ${userAppointment[i]._id}`);
+            doc.text(`Patient Name: ${userAppointment[i].user}`);
+            doc.text(`Patient Exam: ${userAppointment[i].exam}`);
+            doc.text(`Payment Method: ${userAppointment[i].payment}`);
+            doc.text(`Date: ${userAppointment[i].date}`);
+            doc.moveDown();
+          
+        }    
+        // Footer
+        doc.text('Thank you for choosing our service!', { align: 'center' });
+      
+        doc.end();
+      
+        console.log(`PDF created: ${filePath}`);
+    }
+
     module.exports = 
     {
     booking,
-    getAppointments
+    getAppointments,
+    printExam
     }
